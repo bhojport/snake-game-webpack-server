d664e3a1231e2090acc5fb9039426df0046bbff4
Bhojendra Rauniyar <bhojendra@danfecreatives.com>
2017-10-18 16:24:21 +0545

    misconfig fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 var path = require("path");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var FaviconWebpackPlugin = require("favicons-webpack-plugin");
+// var HtmlWebpackPlugin = require("html-webpack-plugin");
+// var webpack = require("webpack");
+
 
 var extractPlugin = new ExtractTextPlugin({
     filename: "game.css"
@@ -56,6 +58,8 @@ module.exports = {
         // new webpack.optimize.UglifyJsPlugin({
 
         // })
+        // new webpack.optimize.UglifyJsPlugin(),
+        // new HtmlWebpackPlugin({template: "./index.html"}), // For distributing html file, "on dist folder for eg."
         extractPlugin
     ]
 }
